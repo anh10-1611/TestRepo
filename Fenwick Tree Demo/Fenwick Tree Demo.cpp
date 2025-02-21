94b9c496fdc4b794fb4baefddea48dcf760d57f8
anh10-1611 <triet.1611nguyen@gmail.com>
2025-02-21 07:31:41 +0700

    add comments

--- Fenwick Tree Demo/Fenwick Tree Demo.cpp
+++ Fenwick Tree Demo/Fenwick Tree Demo.cpp
@@ -3,32 +3,42 @@
 
 using namespace std;
 
-
+//This is the function to assign all elements of fwTree to 0 to make it easier to create and adjust
 void deploy(vector<int> fTree) {
 	for (int i = 0; i < fTree.size(); i++) {
 		fTree[i] = 0;
 	}
 }
 
+//This is the function to increase elements within the 2's complement method 
 void increment(vector<int> &fTree, int i, int val) {
+	//We start from 1 instead of 0 so adding 1 to index 
 	i++;
 	while (i < fTree.size()) {
 		fTree[i] += val;
+		//This is increasing index by flipping the bits, and it to the original then add it to the index
 		i += i & -i;
 	}
 
 }
 
-int sumZerotoN(vector<int> fTree, int n) {
+//This is the function to calculate the sum from element 0 to element N in the initial array
+int sumZeroToN(vector<int> fTree, int n) {
 	int sum = 0;
 	while (n >= 1) {
 		sum += fTree[n];
+		//This is decreasing index by flipping the bits, and it to the original then subtract it from the index
 		n -= n & -n;
 	}
 	return sum;
 }
 
+//This is the function to calculate the sum from element b to element e in the initial array by subtracting sumZeroTo_b-1 from sumZeroTo_e
+int sumUp(vector<int> fTree, int b, int e) {
+	return sumZeroToN(fTree, e) - sumZeroToN(fTree, b - 1);
+}
 
+//This is the function to printout all elements in the tree
 void display(vector<int> fTree) {
 	for (int i = 1; i < fTree.size(); i++) {
 		cout << "Index[" << i << "]= " << fTree[i] << "\n";
@@ -36,9 +46,6 @@ void display(vector<int> fTree) {
 }
 
 
-int sumUp(vector<int> fTree, int b, int e) {
-	return sumZerotoN(fTree, e) - sumZerotoN(fTree, b - 1);
-}
 
 int main() {
 	int n;
@@ -51,15 +58,17 @@ int main() {
 	}
 	vector<int> fwTree(n + 1);
 	deploy(fwTree);
-
+	
+	//This is used to add elements to the Fenwick tree
 	for (int i = 0; i < arr.size(); i++) {
 		increment(fwTree, i, arr[i]);
 	}
+
+
 	display(fwTree);
 
-	int sum = sumZerotoN(fwTree, n);
+	int sum = sumZeroToN(fwTree, n);
 	cout << "sum from 0 to " << n << " is " << sum;
-
 	int sum2 = sumUp(fwTree, 3, 8);
 	cout << "\nsum from 3 to 8 is " << sum2;
 
