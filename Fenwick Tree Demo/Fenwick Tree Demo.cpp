10a85aa2a2885a9d7ae7477ebdc0f004cce8e63c
anh10-1611 <triet.1611nguyen@gmail.com>
2025-02-21 17:34:31 +0700

    This serve as a test1 program

--- Fenwick Tree Demo/Fenwick Tree Demo.cpp
+++ Fenwick Tree Demo/Fenwick Tree Demo.cpp
@@ -4,6 +4,83 @@
 using namespace std;
 
 //This is the function to assign all elements of fwTree to 0 to make it easier to create and adjust
+void deploy(vector<int> fTree);
+
+//This is the function to increase elements within the 2's complement method 
+void increment(vector<int>& fTree, int i, int val);
+
+//This is the function to calculate the sum from element 0 to element N in the initial array
+int sumZeroToN(vector<int> fTree, int n);
+
+//This is the function to calculate the sum from element b to element e in the initial array by subtracting sumZeroTo_b-1 from sumZeroTo_e
+int sumUp(vector<int> fTree, int b, int e);
+
+//This is the function to printout all elements in the tree
+void display(vector<int> fTree);
+
+void menu() {
+	cout << "1: Find the sum of (0, n)\n";
+	cout << "2: Find the sum of (m, n)\n";
+	cout << "0: Exit program!\n";
+	cout << "Please select your program!\n";
+}
+
+int main() {
+	int n;
+	cout << "how many numbers in the array? "; cin >> n;
+	vector<int> arr(n);
+	cout << "\nPlease input your array\n";
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	vector<int> fwTree(n + 1);
+	deploy(fwTree);
+	
+	//This is used to add elements to the Fenwick tree
+	for (int i = 0; i < arr.size(); i++) {
+		increment(fwTree, i, arr[i]);
+	}
+
+
+	display(fwTree);
+
+	int x = 0;
+	int control;
+	menu();
+
+	cin >> control;
+
+	while (control > 0 && control < 3) {
+		switch (control) {
+		case(1): {
+			int e;
+			cout << "Input n: "; cin >> e;
+			int sum = sumZeroToN(fwTree, e);
+			cout << "Sum(0, " << e << ") = " << sum;
+			cout << "\n";
+			break;
+		}
+		case(2): {
+			int b, e;
+			cout << "Input m: "; cin >> b;
+			cout << "Input n: "; cin >> e;
+			int sum = sumUp(fwTree, b, e);
+			cout << "Sum(" << b << ", " << e << ") = " << sum;
+			cout << "\n";
+			break;
+		}
+		default:
+			break;
+		}
+		cout << "Which program you want to execute next?\n";
+		cin >> control;
+	}
+
+	return 0;
+}
+
+//This is the function to assign all elements of fwTree to 0 to make it easier to create and adjust
 void deploy(vector<int> fTree) {
 	for (int i = 0; i < fTree.size(); i++) {
 		fTree[i] = 0;
@@ -11,7 +88,7 @@ void deploy(vector<int> fTree) {
 }
 
 //This is the function to increase elements within the 2's complement method 
-void increment(vector<int> &fTree, int i, int val) {
+void increment(vector<int>& fTree, int i, int val) {
 	//We start from 1 instead of 0 so adding 1 to index 
 	i++;
 	while (i < fTree.size()) {
@@ -19,7 +96,6 @@ void increment(vector<int> &fTree, int i, int val) {
 		//This is increasing index by flipping the bits, and it to the original then add it to the index
 		i += i & -i;
 	}
-
 }
 
 //This is the function to calculate the sum from element 0 to element N in the initial array
@@ -43,34 +119,4 @@ void display(vector<int> fTree) {
 	for (int i = 1; i < fTree.size(); i++) {
 		cout << "Index[" << i << "]= " << fTree[i] << "\n";
 	}
-}
-
-
-
-int main() {
-	int n;
-	cout << "how many numbers in the array? "; cin >> n;
-	vector<int> arr(n);
-	cout << "\nPlease input your array\n";
-
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	vector<int> fwTree(n + 1);
-	deploy(fwTree);
-	
-	//This is used to add elements to the Fenwick tree
-	for (int i = 0; i < arr.size(); i++) {
-		increment(fwTree, i, arr[i]);
-	}
-
-
-	display(fwTree);
-
-	int sum = sumZeroToN(fwTree, n);
-	cout << "sum from 0 to " << n << " is " << sum;
-	int sum2 = sumUp(fwTree, 3, 8);
-	cout << "\nsum from 3 to 8 is " << sum2;
-
-	return 0;
 }
